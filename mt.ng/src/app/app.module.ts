dfab155b2575dd78efb854476ccfc35ff3cbde3c
denisramiros <denisramiros@gmail.com>
2020-06-05 15:19:57 +0200

    Update angular v, add material library

--- mt.ng/src/app/app.module.ts
+++ mt.ng/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
