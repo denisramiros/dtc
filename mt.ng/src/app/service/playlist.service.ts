cc58aeeefdc232732c69c932a9969e87e20b24c7
denisramiros <denisramiros@gmail.com>
2020-07-01 14:25:31 +0200

    Remove amplify | add firebase

--- mt.ng/src/app/service/playlist.service.ts
+++ mt.ng/src/app/service/playlist.service.ts
@@ -1,27 +1,30 @@
-import { RequestService } from './request.service';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Song } from '../model/song.interface';
-import { Playlist } from '../model/playlist.interface';
+import { RequestService } from "./request.service";
+import { Injectable } from "@angular/core";
+import { Observable, NEVER } from "rxjs";
+import { Song } from "../model/song.interface";
+import { Playlist } from "../model/playlist.interface";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class PlaylistService {
-    public constructor(private requestService: RequestService) {}
+  public constructor(private requestService: RequestService) {}
 
-    public addSong(song: Song): Observable<Song> {
-        console.log('addSong send', JSON.stringify(song));
-        return this.requestService.postRequest<Song>('http://localhost:54536/playlist', JSON.stringify(song));
-    }
+  public addSong(song: Song): Observable<Song> {
+    // console.log('addSong send', JSON.stringify(song));
+    // return this.requestService.postRequest<Song>('http://localhost:54536/playlist', JSON.stringify(song));
+    return NEVER;
+  }
 
-    public getPlaylist(): Observable<Playlist> {
-        return this.requestService.getRequest<Playlist>('http://localhost:54536/playlist');
-    }
+  public getPlaylist(): Observable<Playlist> {
+    // return this.requestService.getRequest<Playlist>('http://localhost:54536/playlist');
+    return NEVER;
+  }
 
-    public deletePlaylist(): Observable<Playlist> {
-        return this.requestService.deleteRequest<Playlist>('http://localhost:54536/playlist');
-    }
+  public deletePlaylist(): Observable<Playlist> {
+    // return this.requestService.deleteRequest<Playlist>('http://localhost:54536/playlist');
+    return NEVER;
+  }
 
-    public getCurrentSong() {}
+  public getCurrentSong() {}
 }
